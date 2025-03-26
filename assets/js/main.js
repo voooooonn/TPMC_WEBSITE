61ef813b5af4ebd912cbbb21abb26b66a6b559ed
voooooonn <vonquiatchon@gmail.com>
2025-03-26 15:56:23 +0800

    initial commit

---
+++ assets/js/main.js
@@ -0,0 +1,25 @@
+// ...existing code...
+var swiper = new Swiper('.swiper-container', {
+    slidesPerView: 1,
+    spaceBetween: 10,
+    loop: true,
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+    pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+    },
+    breakpoints: {
+        640: {
+            slidesPerView: 2,
+            spaceBetween: 20,
+        },
+        768: {
+            slidesPerView: 3,
+            spaceBetween: 30,
+        },
+    },
+});
+// ...existing code...
